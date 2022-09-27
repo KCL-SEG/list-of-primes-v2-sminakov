bca1703f5d0e4eb661db2c2a67a327005859437b
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2022-09-27 23:01:05 +0100

    complete

--- primes.py
+++ primes.py
@@ -2,5 +2,40 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError("the number of primes must be greater than 0")
+    numbers = [2]
     list = []
+    counter=0
+    while counter<number_of_primes:
+        if counter==0:
+            list.append(2)
+            counter+=1
+        else:
+            list.append(findPrime(list[len(list)-1], numbers))
+            counter+=1
     return list
+
+def findPrime(start, numbers):
+    found = False
+    num = start
+    while not found:
+        num+=1
+        if not divisible(num, numbers):
+            found = True
+        numbers.append(num)
+    return num
+
+def divisible(num, numbers):
+    result = False
+    counter = len(numbers)-1
+    while counter>=0:
+        # print(num)
+        # print(numbers[counter])
+        # print(num%numbers[counter])
+        if num%numbers[counter] == 0:
+            result = True
+            break
+        else:
+            counter-=1
+    return result
